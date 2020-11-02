6118bd9170b4d12d947ecdcfe7f3bfd7297ec946
levon-avackimyanc <levon.avakimyanc.01@mail.ru>
2020-11-02 21:58:51 +0300

    Updated

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,18 @@
-// Copyright 2018 Your Name <your_email>
-
+//
+//  Copyright 2020 Avakimyanc Levon levon.avakimyanc.01@mail.ru
+//
 #include <gtest/gtest.h>
 
-TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+#include <iostream>
+
+#include "Cache_Exp.hpp"
+
+TEST(Cache_Exp, Main) {
+  Cache_Exp C;
+  C.Direct_variation();
+  std::cout << C;
+  C.Reverse_variation();
+  std::cout << C;
+  C.Random_variation();
+  std::cout << C;
 }
